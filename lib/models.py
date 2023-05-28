230dad4437502343f4a0d1c059999cefc913d3b1
manoti <manotimike@gmail.com>
2023-05-28 10:05:31 +0300

    done

--- lib/models.py
+++ lib/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, func
 from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -20,6 +21,8 @@ class Game(Base):
     price = Column(Integer())
 
     reviews = relationship('Review', backref=backref('game'))
+    users = association_proxy('reviews', 'user',
+        creator=lambda us: Review(user=us))
 
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
@@ -35,7 +38,27 @@ class Review(Base):
     
     game_id = Column(Integer(), ForeignKey('games.id'))
 
+    user_id = Column(Integer(), ForeignKey('users.id'))
+    
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
             f'score={self.score}, ' + \
             f'game_id={self.game_id})'
+    
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    reviews = relationship('Review', backref=backref('user'))
+    games = association_proxy('reviews', 'game',
+        creator=lambda gm: Review(game=gm))
+
+    # don't forget your __repr__()!
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
